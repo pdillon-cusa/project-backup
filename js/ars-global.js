ae8d2ffe6e8b6ce17c29554b279b8429a5b34bda
Peter Dillon <q12721@corp.cusa.canon.com>
2018-07-06 09:57:21 -0400

    modal and order history

--- js/ars-global.js
+++ js/ars-global.js
@@ -11,33 +11,38 @@ $(document).keydown(function(e) {
 	}
 });
 
+// ----------- Cancel Service Modal --------------------
+function cancelServiceDialog() {
+	$('#cancelServiceModal').show();
+	$(".overlay").css('display', 'block');
+}
+
+
+//--------------- Page Loading Items --------------
+$(".overlay").css('display', 'block');
+$(".loading").css('display', 'block');
+
+function loaded() {
+    $(".overlay").fadeOut(200);
+    $(".loading").fadeOut(200);
+    $(".custom-container").css('visibility','visible').hide().fadeIn(200);
+}
+
+//-------------- Close Any Modal --------------------
+function closeModal() {
+    $(".overlay").fadeOut(200);
+    $(".custom-modal").fadeOut(200);
+}
+$(document).keydown(function(e) {
+    if (e.keyCode == 27) { closeModal() }
+});
+$(".close-overlay").on("click", function () {
+    closeModal();
+});
 
 
 
 
-
-// //--------------- Page Loading Items --------------
-// $(".overlay").css('display', 'block');
-// $(".loading").css('display', 'block');
-
-// function loaded() {
-//     $(".overlay").fadeOut(200);
-//     $(".loading").fadeOut(200);
-//     $(".custom-container").css('visibility','visible').hide().fadeIn(200);
-// }
-
-// //-------------- Close Any Modal --------------------
-// function closeModal() {
-//     $(".overlay").fadeOut(200);
-//     $(".custom-modal").fadeOut(200);
-//     $("#maxTooltip").fadeOut(200);
-// }
-// $(document).keydown(function(e) {
-//     if (e.keyCode == 27) { closeModal() }
-// });
-// $(".close-overlay").on("click", function () {
-//     closeModal();
-// });
 
 // //--------- Drop Down Datatlist and Type-Ahead --------------
 // (function(win,doc) {
